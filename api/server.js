63bed93053e3d59e2b6dccb41877ff2c92a455db
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-28 10:25:28 -0500

    finished the delete and put for todos

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use(express.json())
 
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
-server.use('/api/todos', todosRouter)
+server.use('/api', todosRouter)
 
 server.get('/', (req, res) => {
   res.json({ api: 'up' })
